ff6648c7a275b812c633c125981680424f6679b3
bobqiu <xiaoqiu369@gmail.com>
2015-12-22 20:23:34 +0800

    init

---
+++ src/main/java/org/appfuse/tutorial/dao/SearchException.java
@@ -0,0 +1,13 @@
+package org.appfuse.tutorial.tutorial.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
---
+++ src/main/java/org/appfuse/tutorial/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package org.appfuse.tutorial.tutorial.service.impl;
+
+import org.appfuse.tutorial.tutorial.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
